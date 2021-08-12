f407312b016e96dcc4daf6dd901cb9629d24805e
Anifyuli <anifyuliansyah@gmail.com>
2021-08-12 21:37:51 +0700

    update landing page

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <!-- Bootstrap JS, Popper & JQuery -->
@@ -12,13 +14,81 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     <title>Beranda</title>
 </head>
-<body>
-    <!-- header -->
+<body class="text-dark">
 
-    <!-- welcome-section -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><span class="text-success">Gawean</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <!-- strength of gawean, icons, and -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mx-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Kelas
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Pekerja</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Prakerja</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">UMKM</a>
+            </div>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Berita</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="">Tentang</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
 
-    <!--  -->
+    <div class="my-3 container-fluid mx-5" style="height : 4 rem; width : auto;">
+        <div class="row">
+            <div class="col">
+                <h1 class="my-3">Maju dan berkembang serta membangun insan kreatif bersama Gawean</h1>
+                <h4 class="my-2">Dapatkan informasi, kelas atraktif untuk meningkatkan produktivitas diri dalam melihat peluang usaha di sekitar Anda</h4>
+                <a class="mt-3 btn btn-success" href="">Ayo gabung<span class="ml-4 fas fa-arrow-right"></span></a>
+            </div>
+            <div class="col bg-dark">
+
+            </div>
+        </div>
+    </div>
+
+    <div style="height : 6 rem; width : auto;" class="container-md mx-lg mt-5">
+        <div>
+            <h1>Dapatkan informasi, kelas atraktif untuk meningkatkan produktivitas diri dalam melihat peluang usaha di sekitar Anda</h1>
+            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eligendi cupiditate dignissimos. Odio, et blanditiis? Id, asperiores velit quo libero, harum ducimus iusto modi quae corrupti laborum aperiam! Sint, voluptas?
+            Deserunt facere ea, veritatis recusandae quasi voluptate praesentium vel. Aperiam aspernatur libero laboriosam? Praesentium ipsa iusto nobis eligendi sint deleniti corporis. Dolores culpa nihil consequuntur facilis ipsum nulla illo nobis!
+            Labore facere harum voluptatum, reiciendis possimus libero, dicta eveniet beatae quasi ipsam doloribus sint. Omnis nulla et provident temporibus fugiat porro nemo eos nihil. Necessitatibus deserunt quam similique molestiae voluptates!</h5>
+        </div>
+        <div class="row">
+            <div class="col-2 mx-4 bg-success">
+                <span class="fas fa-cogs"></span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dolor possimus quis, magni sequi recusandae. Fugit accusantium porro deserunt, dicta possimus in at aperiam recusandae sed quidem iusto illo beatae?</p>
+            </div>
+            <div class="col border border-dark">
+                <span class="fas fa-cogs"></span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dolor possimus quis, magni sequi recusandae. Fugit accusantium porro deserunt, dicta possimus in at aperiam recusandae sed quidem iusto illo beatae?</p>
+            </div>
+            <div class="col border border-dark">
+                <span class="fas fa-cogs"></span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dolor possimus quis, magni sequi recusandae. Fugit accusantium porro deserunt, dicta possimus in at aperiam recusandae sed quidem iusto illo beatae?</p>
+            </div>
+            <div class="col border border-dark">
+                <span class="fas fa-cogs"></span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dolor possimus quis, magni sequi recusandae. Fugit accusantium porro deserunt, dicta possimus in at aperiam recusandae sed quidem iusto illo beatae?</p>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
